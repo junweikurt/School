52881fdbd31b0dac3bde0feb3bb6629878ee8880
Kurt <43737791+junweikurt@users.noreply.github.com>
2022-01-29 21:43:58 +0800

    Add files via upload

---
+++ Loan.java
@@ -0,0 +1,55 @@
+import java.util.Date;
+import java.lang.Math;
+public class Loan {
+
+    private double annualInterestRate;
+    private int numberOfYears;
+    private double loanAmount;
+    private Date loanDate;
+
+
+    public Loan(){
+
+    }
+
+    public Loan(double annualInterestRate, int numberOfYears, double loanAmount){
+        this.annualInterestRate = annualInterestRate;
+        this.numberOfYears = numberOfYears;
+        this.loanAmount = loanAmount;
+    }
+
+    public double getAnnualInterestRate(){
+        return this.annualInterestRate;
+    }
+    
+    public int getNumberOfYears(){
+        return this.numberOfYears;
+    }
+    
+    public double getLoanAmount(){
+        return this.loanAmount;
+    }
+    public Date getLoanDate(){
+        return this.loanDate;
+    }
+    public void setAnnualInterestRate(double annualInterestRate){
+        this.annualInterestRate = annualInterestRate;
+    }
+    public void setNumberOfYears(int numberOfYears){
+        this.numberOfYears = numberOfYears;
+    }
+    public void setLoanAmount(double loanAmount){
+        this.loanAmount = loanAmount;
+    }
+    public double getMonthlyPayment(){
+        double month = (this.annualInterestRate/12)/100;
+        double a = this.loanAmount * month;
+        double b = 1 - (1/ Math.pow((1+month),(this.numberOfYears * 12)));
+        double monthlypayment =  a/b;
+        return monthlypayment;
+    }
+    public double getTotalPayment(){
+        double totalpayment = getMonthlyPayment() * this.numberOfYears *12;
+        return totalpayment;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+import java.util.Scanner;
+public class Main {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter annual interest rate, for example, 8.25: ");
+        float interestrate= input.nextFloat();
+        System.out.print("Enter number of years as an integer: ");
+        int years = input.nextInt();
+        System.out.print("Enter loan amount, for example, 120000.95: ");
+        float loanamount= input.nextFloat();
+        Loan newloan = new Loan(interestrate,years,loanamount);
+        Date date = new Date();
+        System.out.println("The loan was created on " + date);
+        System.out.println("The monthly payment is " + String.format("%.2f",newloan.getMonthlyPayment()));
+        System.out.println("The total payment is " + String.format("%.2f",newloan.getTotalPayment()));
+    }
+}
